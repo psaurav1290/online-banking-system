949852b95864ba1295cc1cd936a8c7cc2634c86f
PotatoinPyjamas <kumaris4878@gmail.com>
2023-09-19 21:25:32 +0530

    added: exceptions

---
+++ backend/src/main/java/com/banking/bank/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.banking.bank.exceptions;
+
+//@ResponseStatus(value=Ht/tpStatus.NOT_FOUND,reason = "Invalid Employee Id")
+public class ResourceNotFoundException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
