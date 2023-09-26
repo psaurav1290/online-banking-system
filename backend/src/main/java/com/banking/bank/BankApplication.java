f010d608b0c64fc344969ae0ebf022732ad4decc
Aditya Dubey <dubeyaditya6232@gmail.com>
2023-09-26 23:16:06 +0530

    added: swagger ui

--- backend/src/main/java/com/banking/bank/BankApplication.java
+++ backend/src/main/java/com/banking/bank/BankApplication.java
@@ -2,9 +2,17 @@ package com.banking.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BankApplication {
+
+	@GetMapping("/")
+	String home() {
+		return "API is working";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankApplication.class, args);
